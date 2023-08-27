43bc9e444baf5423d20dda1a1f1d5b4214bf5094
AhmedEhab-SG <ahmedehab.sg@gmail.com>
2023-08-28 01:35:15 +0300

    adj footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,18 +2,23 @@
 
 import { FaGithub } from "react-icons/fa";
 import ClientOnly from "./shared/ClientOnly";
+import Image from "next/image";
+import logo from "../assets/airbnb-logo.png";
 
 const Footer = () => {
   return (
     <ClientOnly>
       <footer className="flex flex-col md:flex-row justify-evenly items-center bg-slate-200 w-full p-2 font-semibold text-gray-700 text-lg">
-        <div className="mt-1">Next-Airbnbtsx.</div>
+        <div className="flex justify-center items-center gap-2 mt-1 text-rose-500 font-bold">
+          <Image src={logo} className="w-8" alt="airbnb-logo" />
+          <div className="">next-airbnb</div>
+        </div>
         <a href="https://github.com/AhmedEhab-SG" target="_blank">
           <div className="m-3">Built By Ahmed Ehab.</div>
         </a>
         <a href="https://github.com/AhmedEhab-SG/next-airbnb" target="_blank">
           <div className="flex justify-center items-center gap-2">
-            <div className="mb-1">Interested? Feel Free.</div>
+            <div className="mb-1 text-gray-500">Interested? Feel Free.</div>
             <FaGithub size={25} />
           </div>
         </a>
